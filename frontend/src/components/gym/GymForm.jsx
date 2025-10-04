8d960e87e46b64a49e721de536acb5ca83f10379
Alejsv91 <Alejandro91_cr@hotmail.com>
2025-10-03 18:36:44 -0600

    SCRUM-7 last validation changes

--- frontend/src/components/gym/GymForm.jsx
+++ frontend/src/components/gym/GymForm.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Gym } from "../../types/gym";
+import {
+  isAlphabetic,
+  isNumeric,
+  containsSpecialCharacters,
+  isEmail,
+} from "../../utils/validationUtils";
 
 const GymForm = ({ initialValues = {}, mode = "create", onSubmit }) => {
   const gym = new Gym(
@@ -21,23 +27,92 @@ const GymForm = ({ initialValues = {}, mode = "create", onSubmit }) => {
       name: "legalId",
       label: "Legal Id",
       validations: {
-        alphabetic: false,
-        special: false,
         numeric: true,
         minLength: 9,
       },
     },
-    { name: "name", label: "Name" },
-    { name: "owner", label: "Owner" },
-    { name: "phone", label: "Phone" },
-    { name: "email", label: "Email" },
+    {
+      name: "name",
+      label: "Name",
+      validations: {
+        minLength: 1,
+      },
+    },
+    {
+      name: "owner",
+      label: "Owner",
+      validations: {
+        alphabetic: true,
+        special: false,
+        numeric: false,
+        minLength: 9,
+      },
+    },
+    {
+      name: "phone",
+      label: "Phone",
+      validations: {
+        numeric: true,
+        minLength: 8,
+      },
+    },
+    { name: "email", label: "Email", validations: { isEmail: true } },
   ];
+  const [formErrors, setFormErrors] = useState({});
   const checkbox = [{ name: "isActive", label: "Is Active" }];
   const dateTimeInput = [{ name: "activationDate", label: "Activation Date" }];
 
-  const handleChange = (e, validations) => {
+  const handleChange = (e, newInput) => {
     const { name, value, type, checked } = e.target;
     const newValue = type === "checkbox" ? checked : value;
+
+    if (
+      newInput.validations["alphabetic"] !== undefined &&
+      newInput.validations["alphabetic"] &&
+      !isAlphabetic(value)
+    ) {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: `${newInput.label} required alphabetic characters only`,
+      }));
+    } else {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: "",
+      }));
+    }
+
+    if (
+      newInput.validations["numeric"] !== undefined &&
+      newInput.validations["numeric"] &&
+      !isNumeric(value)
+    ) {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: `${newInput.label} required numeric characters only`,
+      }));
+    } else {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: "",
+      }));
+    }
+
+    if (
+      newInput.validations["isEmail"] !== undefined &&
+      newInput.validations["isEmail"] &&
+      !isEmail(value)
+    ) {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: `Please add a valid email`,
+      }));
+    } else {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: "",
+      }));
+    }
 
     setFormData((prev) => ({
       ...prev,
@@ -73,10 +148,15 @@ const GymForm = ({ initialValues = {}, mode = "create", onSubmit }) => {
             id={newInput.name}
             name={newInput.name}
             value={formData.gym[newInput.name]}
-            onChange={handleChange}
+            onChange={(e) => handleChange(e, newInput)}
             className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:ring-blue-500 focus:border-blue-500"
             required
           />
+          {formErrors[newInput.name] && (
+            <p className="text-red-500 text-sm mt-1">
+              {formErrors[newInput.name]}
+            </p>
+          )}
         </div>
       ))}
       {checkbox.map((field) => (
---
+++ frontend/src/utils/validationUtils.js
@@ -0,0 +1,15 @@
+export function isAlphabetic(str) {
+  return /^[A-Za-z]+$/.test(str);
+}
+
+export function isEmail(str) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
+}
+
+export function isNumeric(str) {
+  return /^[0-9]+$/.test(str);
+}
+
+export function containsSpecialCharacters(str) {
+  return /[^a-zA-Z0-9]/.test(str);
+}
